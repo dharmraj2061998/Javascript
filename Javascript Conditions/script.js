5cba9098be517972e9e5509169d17d22958ca55e
Dharmraj Joshi <dharmrajjoshi2061998@gmail.com>
2022-02-22 00:05:24 +0530

    commit on master

---
+++ Javascript Conditions/script.js
@@ -0,0 +1,23 @@
+// If conditions
+
+/*let age = 19
+let hasvotercard = '121'
+if (age >= 18) {
+  if (hasvotercard == 'yes') {
+    alert('You can vote!')
+  } else {
+    alert(' Get your voter id card')
+  }
+} else {
+  alert('You can not vote')
+} */
+
+//turnary operator
+// let islogin = 0
+// let option = islogin == 1 ? 'logout' : 'login'
+// document.write(option)
+// console.log(option)
+
+// let user
+
+// alert(user ?? 'Guest User')
